12ec867ee0da2923082a1a95f4741fceaa29d468
andreasvidal <andrea.s.vidal00@gmail.com>
2022-06-23 16:06:54 -0400

    segundo ejercicio open bootcamp

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,12 @@
+const aboutMe = {
+    name: 'Andrea',
+    age: 34,
+    developer: true,
+    date: new Date('april 18 1988'),
+    favorite_book: {
+        title: 'mi planta de naranja lima',
+        author: ' José Mauro de Vasconcelos',
+        date: 1968,
+        url: 'https://librosdelasteroide.com/libro/mi-planta-de-naranja-lima'
+    }
+}
